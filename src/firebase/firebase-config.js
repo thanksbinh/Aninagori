e15ba1c2aa0840dd2872cedc9c4c0d4a903e7283
Binh <lethanhbinh6203@gmail.com>
2023-03-02 17:48:44 +0700

    Add NEXT_PUBLIC_ to env variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCE-ZOEqFLjjHIy4jow8sRB33A4moFJQ40",
-    authDomain: "aninagori-0.firebaseapp.com",
-    projectId: "aninagori-0",
-    storageBucket: "aninagori-0.appspot.com",
-    messagingSenderId: "1012719671988",
-    appId: "1:1012719671988:web:00c48b47b89b6c8dfed5e1",
-    measurementId: "G-VXRCWH0XQR"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase-admin/auth"
 import { doc, serverTimestamp, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    appId: process.env.FIREBASE_APP_ID,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 }
 
 export const authOptions: NextAuthOptions = {
@@ -40,8 +40,8 @@ export const authOptions: NextAuthOptions = {
             }
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!,
         }),
         // ...add more providers here
     ],
