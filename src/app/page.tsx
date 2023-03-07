22d5aebf936276def74529bb4e30c9fcda07882d
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-03-07 10:53:25 +0700

    delete duplicate page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Homes() {
-  return (
-    <Link href="/profile" className="text-3xl no-underline text-center w-max">
-      Go To Profile
-    </Link>
-  );
-}
