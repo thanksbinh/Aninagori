80456cfde20abe71599e4915e41fe44a4e44f2e0
Binh <lethanhbinh6203@gmail.com>
2023-03-09 11:51:15 +0700

    Add friend accept btn in profile page

--- src/app/user/[user_name]/profileComponent/ProfileHeader/AddFriendBtn.tsx
+++ src/app/user/[user_name]/profileComponent/ProfileHeader/AddFriendBtn.tsx
@@ -10,25 +10,34 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function AddFriendBtn({ myUserInfo, userInfo }: { myUserInfo: UserInfo, userInfo: UserInfo }) {
   const [content, setContent] = useState("Add friend")
+  const [disabled, setDisabled] = useState(false)
 
   useEffect(() => {
     // If requested
-    if ((userInfo as any)?.friend_request_list?.some((acc: any) => (acc.username === myUserInfo.username)))
+    if ((userInfo as any)?.friend_request_list?.some((acc: any) => (acc.username === myUserInfo.username))) {
       setContent("Requesting...")
+    }
     // If friend
-    if ((userInfo as any)?.friend_list?.some((acc: any) => (acc.username === myUserInfo.username)))
+    if ((userInfo as any)?.friend_list?.some((acc: any) => (acc.username === myUserInfo.username))) {
       setContent("Friend")
+    }
+    // If requesting
+    if ((myUserInfo as any)?.friend_request_list?.some((acc: any) => (acc.username === userInfo.username)))
+      setContent("Accept friend")
   }, [])
 
+  useEffect(() => {
+    if (content === "Requesting..." || content === "Friend") setDisabled(true);
+    else setDisabled(false);
+  }, [content])
+
   const handleAddFriend = async () => {
-
-    setContent("Requesting...")
-
     const docRef = doc(db, "users", myUserInfo.id);
     const docSnap = await getDoc(docRef)
 
     if (docSnap.exists() && docSnap.data().friend_request_list?.map((doc: FriendRequest) => doc.username).includes(userInfo.username)) {
       beFriends(myUserInfo, userInfo)
+      setContent("Friend")
       docSnap.data().friend_request_list.forEach((doc: FriendRequest) => {
         if (doc.username === userInfo.username) {
           removeFriendRequest(myUserInfo, doc)
@@ -36,11 +45,12 @@ export default function AddFriendBtn({ myUserInfo, userInfo }: { myUserInfo: Use
       })
     } else {
       makeFriendRequest(myUserInfo, userInfo.id)
+      setContent("Requesting...")
     }
   }
 
   return (
-    <Button small primary leftIcon={<FontAwesomeIcon icon={faUserPlus as any} />} onClick={handleAddFriend} disabled={content === "Add friend" ? false : true} to={undefined} href={undefined} rightIcon={undefined}>
+    <Button small primary leftIcon={<FontAwesomeIcon icon={faUserPlus as any} />} onClick={handleAddFriend} disabled={disabled} to={undefined} href={undefined} rightIcon={undefined}>
       {content}
     </Button>
   )
