4582b983be78d039d9e19ee786565da343c20a64
Trần Tuấn Anh <tuananhniichan@gmail.com>
2023-03-10 15:35:18 +0700

    Add post to Firestore
    PostForm function to add post to Firestore

--- src/components/Post/PostForm.tsx
+++ src/components/Post/PostForm.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 import Avatar from "../Avatar/Avatar";
-import Image from "next/image";
 import { HiPhoto, HiVideoCamera } from "react-icons/hi2";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { db, storage } from "@/firebase/firebase-app";
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 
 type PostFormProps = {
   authorName: string;
@@ -22,12 +26,46 @@ const PostForm: FC<PostFormProps> = ({
   likes,
   comments,
 }) => {
+  const {data: session} = useSession();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const filePickerRef = useRef<HTMLInputElement>(null);
+  const [imageToPost, setImageToPost] = useState<string | null>(null);
+
+  const handleClear = () => {
+    if (inputRef.current) {
+      inputRef.current.value = ""; // clear input value
+    }
+  }
+
+  async function handleClick(event: React.MouseEvent<HTMLButtonElement>): Promise<void> {
+    // your async logic here
+    event.preventDefault();
+    if (!inputRef.current?.value) return;
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+        authorName: session?.user?.name,
+        avatarUrl: session?.user?.image,
+        timeStamp: serverTimestamp(),
+        content: inputRef.current.value,
+        imgUrl: '',
+        likes: 0,
+        comments: 0
+    });
+
+    console.log("Document written with ID: ", docRef.id);
+
+    handleClear();
+
+    alert("Post successfully added");
+  }
+
   return (
     <div className="flex flex-col flex-1 bg-[#191c21] rounded-2xl p-2 my-4">
       <div className="flex justify-between items-center mt-4">
         <Avatar imageUrl={avatarUrl} altText={authorName} size={8} />
         <input
           type="text"
+          ref={inputRef}
           className="flex rounded-3xl py-3 px-4 mx-2 w-full focus:outline-none bg-[#212833] caret-white"
           placeholder="Share your favourite Animemory now!"
         />
@@ -42,7 +80,7 @@ const PostForm: FC<PostFormProps> = ({
           <HiVideoCamera className="w-5 h-5 fill-[#FF1D43]" />
           <span>Video</span>
         </button>
-        <button className="flex flex-1 items-center justify-center space-x-1 text-[#fff] bg-[#377dff] hover:bg-[#0e5ef1] py-2 px-4 rounded-lg mt-1 mx-1">
+        <button className="flex flex-1 items-center justify-center space-x-1 text-[#fff] bg-[#377dff] hover:bg-[#0e5ef1] py-2 px-4 rounded-lg mt-1 mx-1" onClick={handleClick}>
           <span>Share</span>
         </button>
       </div>
--- src/firebase/firebase-app.js
+++ src/firebase/firebase-app.js
@@ -1,5 +1,6 @@
 import { getApps, getApp, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import firebaseConfig from "./firebase-config";
 import { getAuth } from "firebase/auth"
 import firebase from "firebase/compat/app";
@@ -7,6 +8,7 @@ import firebase from "firebase/compat/app";
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
+const storage = getStorage(app);;
 const auth = getAuth();
 
-export { firebase, app, db, auth }
+export { firebase, app, db, auth, storage }
