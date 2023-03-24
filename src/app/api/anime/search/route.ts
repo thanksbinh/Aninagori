ae130d119949f6f741b646eb2ca918b62cdb76b4
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-03-24 18:09:54 +0700

    test search error

--- src/app/api/anime/search/route.ts
+++ src/app/api/anime/search/route.ts
@@ -6,12 +6,9 @@ export async function GET(request: Request) {
     const search_name: any = request.headers.get("q")
     const offset: any = request.headers.get("offset")
     const limit: any = request.headers.get("limit")
-
-    console.log(search_name, offset, limit)
     const result: any = await getAnimeList(String(search_name), parseInt(offset), parseInt(limit))
-    console.log(result)
     return NextResponse.json(result.data)
   } catch (err) {
-    return NextResponse.json({ error: "cant find anime name" })
+    return NextResponse.json({ error: err })
   }
 }
