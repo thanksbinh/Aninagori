61e4219293ff48583987f88afeb225029997679d
Binh <lethanhbinh6203@gmail.com>
2023-03-29 17:48:04 +0700

    Add updateAvatarUrl method that also update data in friendlists

--- src/app/(home)/components/PostForm.tsx
+++ src/app/(home)/components/PostForm.tsx
@@ -23,26 +23,6 @@ const PostForm: FC<PostFormProps> = ({ username, avatarUrl, className = "", isBa
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, [])
 
-  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
-  //   e.preventDefault()
-  //   if (!inputRef.current?.value && !mediaUrl) return;
-
-  //   const downloadMediaUrl = await uploadMedia();
-  //   await addDoc(collection(db, 'posts'), {
-  //     authorName: username,
-  //     avatarUrl: avatarUrl,
-  //     timestamp: serverTimestamp(),
-  //     content: inputRef.current?.value || "",
-  //     imageUrl: (mediaType === "image") ? downloadMediaUrl : "",
-  //     videoUrl: (mediaType === "video") ? downloadMediaUrl : "",
-  //   });
-
-  //   inputRef.current!.value = "";
-  //   setMediaUrl(null)
-
-  //   router.refresh();
-  // }
-
   function openForm() {
     setTimeout(() => {
       setOpen(true)
@@ -102,27 +82,6 @@ const PostForm: FC<PostFormProps> = ({ username, avatarUrl, className = "", isBa
       <div className="flex flex-col flex-1 bg-ani-black rounded-2xl px-4 my-4">
         <div className="flex justify-between items-center mt-4">
           <Avatar imageUrl={avatarUrl} altText={username} size={8} />
-          {/* <input
-            type="text"
-            ref={inputRef}
-            placeholder="Share your favourite Animemory now!"
-            className="flex rounded-3xl py-3 px-4 mx-2 w-full focus:outline-none bg-ani-gray caret-white"
-          />
-        </div>
-
-
-        <div className="mt-4 w-2/3">
-          {mediaUrl && (
-            mediaUrl.name.endsWith(".mp4") ? (
-              <video src={URL.createObjectURL(mediaUrl)} controls />
-            ) : (
-              <img src={URL.createObjectURL(mediaUrl)} className="w-full object-contain" />
-            )
-          )}
-        </div>
-
-        <div className="flex items-center justify-between py-2 mt-4 mx-2 border-t border-ani-light-gray">
-          <label htmlFor="image-input" className="flex flex-1 items-center justify-center space-x-1 text-[#fff] hover:bg-[#4e5d78] py-2 px-4 rounded-lg mt-1 mx-1 hover:cursor-pointer"> */}
           <div
             onClick={() => {
               openForm()
@@ -154,7 +113,6 @@ const PostForm: FC<PostFormProps> = ({ username, avatarUrl, className = "", isBa
             <span>Video</span>
           </label>
 
-          {/* <button type="submit" className="flex flex-1 items-center justify-center space-x-1 text-[#fff] bg-[#00b4d8] hover:bg-[#0e5ef1] py-2 px-4 rounded-lg mt-1 mx-1"> */}
           <button
             onClick={() => {
               setOpen(true)
--- src/app/user/[user_name]/profileComponent/posts/PostContainer.tsx
+++ src/app/user/[user_name]/profileComponent/posts/PostContainer.tsx
@@ -74,27 +74,28 @@ export default function ProfilePosts({ myUserInfo, profileUsername }: { myUserIn
           animeID={post.post_anime_data?.anime_id}
           postId={post.id}
         >
-          <PostContent
-            authorName={post.authorName}
-            avatarUrl={post.avatarUrl}
-            timestamp={post.timestamp}
-            content={post.content}
-            imageUrl={post.imageUrl}
-            videoUrl={post.videoUrl}
-            animeID={post?.post_anime_data?.anime_id}
-            animeName={post?.post_anime_data?.anime_name}
-            watchingProgress={post?.post_anime_data?.watching_progress}
-            episodesSeen={post?.post_anime_data?.episodes_seen}
-            episodesTotal={post?.post_anime_data?.total_episodes}
-            tag={post?.post_anime_data?.tag}
-            postId={post.id}
-          />
-          <PostAction
-            reactions={post.reactions}
-            commentCountPromise={fetchCommentCount(post.id)}
-            comments={post.lastComment ? [post.lastComment] : []}
-          />
-          <div className="mb-4"></div>
+          <div className="mb-4">
+            <PostContent
+              authorName={post.authorName}
+              avatarUrl={post.avatarUrl}
+              timestamp={post.timestamp}
+              content={post.content}
+              imageUrl={post.imageUrl}
+              videoUrl={post.videoUrl}
+              animeID={post?.post_anime_data?.anime_id}
+              animeName={post?.post_anime_data?.anime_name}
+              watchingProgress={post?.post_anime_data?.watching_progress}
+              episodesSeen={post?.post_anime_data?.episodes_seen}
+              episodesTotal={post?.post_anime_data?.total_episodes}
+              tag={post?.post_anime_data?.tag}
+              postId={post.id}
+            />
+            <PostAction
+              reactions={post.reactions}
+              commentCountPromise={fetchCommentCount(post.id)}
+              comments={post.lastComment ? [post.lastComment] : []}
+            />
+          </div>
         </ContextProvider>
       ))}
 
