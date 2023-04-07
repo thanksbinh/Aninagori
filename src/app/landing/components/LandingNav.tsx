261245d084c88eeb28388baf7964731bf1c6bfec
Trần Tuấn Anh <tuananhniichan@gmail.com>
2023-04-07 16:20:25 +0700

    Add link to signin signup

--- src/app/landing/components/LandingNav.tsx
+++ src/app/landing/components/LandingNav.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/nav/Logo";
+import Link from "next/link";
 
 export default function LandingNav() {
     return (
@@ -8,8 +9,12 @@ export default function LandingNav() {
                     <Logo />
                 </div>
                 <div className="flex items-end justify-end flex-1 pt-2">
-                    <div className="text-white px-4 py-2">Sign in</div>
-                    <div className="text-white px-4 py-2 rounded-xl border border-white">Sign up</div>
+                    <div className="text-white px-4 py-2">
+                        <Link href="/login">Sign in</Link>
+                    </div>
+                    <div className="text-white px-4 py-2 rounded-xl border border-white">
+                        <Link href="/signup">Sign up</Link>
+                    </div>
                 </div>
             </div>
         </div>
