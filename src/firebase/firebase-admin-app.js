51147f19665735582e2a602836a70d2b14ff03a8
Binh <lethanhbinh6203@gmail.com>
2023-04-26 23:52:13 +0700

    Can't fix this build bug without deploying

--- src/firebase/firebase-admin-app.js
+++ src/firebase/firebase-admin-app.js
@@ -1,14 +1,13 @@
 const firebaseAdmin = require('firebase-admin');
 
-if (!firebaseAdmin.apps.length)
-  firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      "projectId": process.env.FIREBASE_PROJECT_ID,
-      "privateKey": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
-    }),
-    databaseURL: "https://aninagori-0.firebaseio.com"
-  });
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "privateKey": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: "https://aninagori-0.firebaseio.com"
+});
 
 const db = firebaseAdmin.firestore();
 
